7219966b778a4bebb28cc85cb95084dea3c16ded
Andres Leon <personalandresll@gmail.com>
2022-09-08 19:41:33 -0500

    modulo sencillo con modulos

---
+++ index.js
@@ -0,0 +1,11 @@
+module.export = {
+    hola() {
+        console.log('Hola mundo!!!');
+    },
+    holaPersonalizado(nombre) {
+        console.log(`Hola ${nombre}`);
+    },
+    holaAlCUrsoNPM(){
+        console.log('Hola al curso de NPM!!');
+    }
+}
