0e230284c5292e11edf47d125b32c18235cd18c4
Andres Leon <personalandresll@gmail.com>
2022-09-09 19:25:52 -0500

    subiendo cambio

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-module.export = {
-    hola() {
-        console.log('Hola mundo!!!');
-    },
-    holaPersonalizado(nombre) {
-        console.log(`Hola ${nombre}`);
-    },
-    holaAlCUrsoNPM(){
-        console.log('Hola al curso de NPM!!');
-    }
+
+
+hola = () => {
+    console.log('Hola mundo!!!');
+    return 1;
 }
+
+holaPersonalizado = (nombre) =>  {
+    console.log(`Hola ${nombre}`);
+    return 1;
+}
+
+holaAlCursoNPM = () => {
+    console.log('Hola al curso de NPM!!');
+    return 1;
+}
+
+module.exports = {
+    hola: hola,
+    holaPersonalizado: holaPersonalizado,
+    holaAlCursoNPM: holaAlCursoNPM
+}
+
+
+
+
+// module.export = {
+//     hola: hola,
+//     holaPersonalizado: holaPersonalizado,
+//     holaAlCUrsoNPM: holaPersonalizado
+// }
